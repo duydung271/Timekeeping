8b6c1c4c74db0847f3a515b15cb520d8867748fe
bravend <bravend.dev@gmail.com>
2022-05-28 17:39:31 +0700

    first

--- static/js/nhansu.js
+++ static/js/nhansu.js
@@ -11,24 +11,12 @@ employeeContainerItems.forEach(function(employeeContainerItem, index){
     }
 })
 
-// them nhan vien js
-
-const selectYear = document.getElementById('select3')
-const date = new Date()
-
-var getYear = ''
-
-for(var i = date.getFullYear(); i >= 1930; --i){
-    getYear += `<option value="${i}">${i}</option>`
-}
-
-selectYear.innerHTML = getYear
-
 // dong mo modal
 const modalBtn = $('.close-btn')
 const modal = $('.modal')
 const modalContainer = $('.modal-container')
 const modalOpenBtns = $$('.employee-show-info')
+
 
 function showModal () {
     modal.classList.add('open')
@@ -38,8 +26,26 @@ function hideModal () {
     modal.classList.remove('open')
 }
 
+
 for(const modalOpenBtn of modalOpenBtns) {
     modalOpenBtn.addEventListener('click', showModal)
+    const employeeId = modalOpenBtn.id
+
+    modalOpenBtn.addEventListener('click', function(){
+        const einfor_string = document.getElementById("einfor_strformat_"+employeeId).textContent
+ 
+        const infors = einfor_string.split('@')
+        
+        document.getElementById("emodal_name").textContent = infors[0]
+        document.getElementById("emodal_gender").textContent = infors[1]
+        document.getElementById("emodal_birthday").textContent = infors[2]
+        document.getElementById("emodal_homeTown").textContent = infors[3]
+        document.getElementById("emodal_phone").textContent = infors[4]
+        document.getElementById("emodal_education").textContent = infors[5]
+        document.getElementById("emodal_avatar").style.background = `url(/media/${infors[6]})  top center / cover no-repeat`
+        // console.log(`url(${infors[6]})  top center / cover no-repeat;`)
+    })
+    
 }
 
 modalBtn.addEventListener('click', hideModal)
@@ -48,3 +54,18 @@ modal.addEventListener('click',hideModal)
 modalContainer.addEventListener('click', function(event){
     event.stopPropagation()
 })
+
+const deleteBtns = $$('.employee-delete')
+for(const deleteBtn of deleteBtns) {
+    deleteBtn.onclick =  async function(){
+        
+        const card= document.getElementById("employee_infor_"+deleteBtn.id);
+        card.remove();
+        const csrfToken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+
+        let res = await fetch(`/employee/?employeeId=${deleteBtn.id}`, {
+            method: "DELETE",
+            headers: {'X-CSRFToken': csrfToken,'Content-Type': 'application/json'}
+            })
+        }
+}
--- static/js/tinhluong.js
+++ static/js/tinhluong.js
@@ -13,6 +13,21 @@ function hideModal () {
 
 for(const modalOpenBtn of modalOpenBtns) {
     modalOpenBtn.addEventListener('click', showModal)
+    const employeeId = modalOpenBtn.id
+    modalOpenBtn.addEventListener('click', function(){
+        const einfor_string = document.getElementById("einfor_strformat_"+employeeId).textContent
+        
+        // console.log(einfor_string)
+        const infors = einfor_string.split('@')
+        
+        document.getElementById("emodal_name").textContent = infors[0]
+        document.getElementById("emodal_birthday").textContent = infors[1]
+        document.getElementById("emodal_phone").textContent = infors[2]
+        document.getElementById("emodal_workday").textContent = infors[3]
+        document.getElementById("emodal_salary").textContent = infors[4]
+        document.getElementById("emodal_avatar").style.background = `url(/media/${infors[5]})  top center / cover no-repeat`
+        // console.log(document.getElementById("emodal_avatar").style.background)
+    })
 }
 
 modalBtn.addEventListener('click', hideModal)
